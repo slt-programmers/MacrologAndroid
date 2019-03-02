6f14eee964625857043dc0ffb85f350f89ea4b6b
Carmen Scholte Lubberink <cscholtelubberink@newnexusmobile.nl>
2019-03-02 10:11:48 +0100

    Bottomnavigation Fragment switch

--- app/src/main/java/com/example/macrologandroid/MainActivity.java
+++ app/src/main/java/com/example/macrologandroid/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.macrologandroid;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import com.example.macrologandroid.Fragments.DiaryFragment;
+import com.example.macrologandroid.Fragments.MealsFragment;
+import com.example.macrologandroid.Fragments.UserFragment;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +22,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_diary:
+                    changeFragment(new DiaryFragment());
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_meals:
+                    changeFragment(new MealsFragment());
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_user:
+                    changeFragment(new UserFragment());
                     return true;
             }
             return false;
@@ -35,10 +40,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        changeFragment(new DiaryFragment());
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragment_content, fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
 
 }
