c2ae14f0dbfed50a6d5868628ebaeb9a9f29d910
Carmen Scholte Lubberink <cscholtelubberink@newnexusmobile.nl>
2019-03-03 15:18:34 +0100

    Styling login page

--- app/src/main/java/com/example/macrologandroid/LoginActivity.java
+++ app/src/main/java/com/example/macrologandroid/LoginActivity.java
@@ -27,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
         // Set up the login form.
         mUserOrEmailView = findViewById(R.id.user_email);
         mPasswordView = findViewById(R.id.password);
