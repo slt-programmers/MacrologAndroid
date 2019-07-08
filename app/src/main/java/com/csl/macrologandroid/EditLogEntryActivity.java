ffedaac429c3fc9e04da3c4b7cc60c174617d723
Carmen Scholte Lubberink <cscholtelubberink@newnexusmobile.nl>
2019-07-08 20:13:15 +0200

    Refactor portion spinner setup, removing duplicate code

--- app/src/main/java/com/csl/macrologandroid/EditLogEntryActivity.java
+++ app/src/main/java/com/csl/macrologandroid/EditLogEntryActivity.java
@@ -8,6 +8,7 @@ import android.database.DataSetObserver;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.csl.macrologandroid.util.SpinnerSetupUtil;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -502,52 +503,13 @@ public class EditLogEntryActivity extends AppCompatActivity {
         });
     }
 
-    private void setupPortionUnitSpinner(String foodname) {
-        for (FoodResponse food : allFood) {
-            if (food.getName().trim().equals(foodname.trim())) {
-                selectedFood = food;
-                break;
-            }
-        }
-
+    private void setupPortionUnitSpinner(String foodName) {
+        SpinnerSetupUtil spinnerUtil = new SpinnerSetupUtil();
+        selectedFood = spinnerUtil.getFoodFromList(foodName, allFood);
         if (selectedFood == null) {
             return;
         }
-
-        List<String> list = new ArrayList<>();
-        for (PortionResponse portion : selectedFood.getPortions()) {
-            String desc = portion.getDescription();
-            if (desc != null && !desc.isEmpty()) {
-                list.add(desc);
-            }
-        }
-        list.add("gram");
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        editPortionOrUnitSpinner.setAdapter(dataAdapter);
-        editPortionOrUnitSpinner.setSelection(0);
-        editPortionOrUnitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (((AppCompatTextView) view).getText().toString().equals("gram")) {
-                    editGramsOrAmount.setInputType(InputType.TYPE_CLASS_NUMBER);
-                    editGramsOrAmount.setText(String.valueOf(100));
-                    editGramsOrAmount.setSelection(3);
-                } else {
-                    editGramsOrAmount.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
-                    editGramsOrAmount.setText(String.valueOf(1));
-                    editGramsOrAmount.setSelection(1);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Not needed
-            }
-        });
-
-        editGramsOrAmount.setVisibility(View.VISIBLE);
+        spinnerUtil.setupPortionUnitSpinner(this, selectedFood, editPortionOrUnitSpinner, editGramsOrAmount);
     }
 
     private void hideSoftKeyboard() {
---
+++ app/src/main/java/com/csl/macrologandroid/util/SpinnerSetupUtil.java
@@ -0,0 +1,71 @@
+package com.csl.macrologandroid.util;
+
+import android.content.Context;
+import android.text.InputType;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.csl.macrologandroid.dtos.FoodResponse;
+import com.csl.macrologandroid.dtos.PortionResponse;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpinnerSetupUtil {
+
+    public FoodResponse getFoodFromList(String foodname, List<FoodResponse> allFood) {
+        for (FoodResponse food : allFood) {
+            if (food.getName().trim().equals(foodname.trim())) {
+                return food;
+            }
+        }
+        return null;
+    }
+
+    public List<String> getPortionList(FoodResponse selectedFood) {
+        List<String> list = new ArrayList<>();
+        for (PortionResponse portion : selectedFood.getPortions()) {
+            String desc = portion.getDescription();
+            if (desc != null && !desc.isEmpty()) {
+                list.add(desc);
+            }
+        }
+        list.add("gram");
+        return list;
+    }
+
+    public void setupPortionUnitSpinner(Context context, FoodResponse selectedFood, Spinner editPortionOrUnitSpinner, TextInputEditText editGramsOrAmount) {
+        List<String> list = getPortionList(selectedFood);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        editPortionOrUnitSpinner.setAdapter(dataAdapter);
+        editPortionOrUnitSpinner.setSelection(0);
+        editPortionOrUnitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (((AppCompatTextView) view).getText().toString().equals("gram")) {
+                    editGramsOrAmount.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    editGramsOrAmount.setText(String.valueOf(100));
+                    editGramsOrAmount.setSelection(3);
+                } else {
+                    editGramsOrAmount.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+                    editGramsOrAmount.setText(String.valueOf(1));
+                    editGramsOrAmount.setSelection(1);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Not needed
+            }
+        });
+
+        editGramsOrAmount.setVisibility(View.VISIBLE);
+    }
+
+}
