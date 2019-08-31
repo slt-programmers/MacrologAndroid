e62a059110afea44b03976e9f9bfa78a7cecb2c2
Carmen Scholte Lubberink <cscholtelubberink@newnexusmobile.nl>
2019-08-31 21:05:39 +0200

    Add strava connection

---
+++ app/src/main/java/com/csl/macrologandroid/dtos/ConnectivityRequest.java
@@ -0,0 +1,28 @@
+package com.csl.macrologandroid.dtos;
+
+public class ConnectivityRequest {
+
+    private String name;
+    private String value;
+
+    public ConnectivityRequest(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
